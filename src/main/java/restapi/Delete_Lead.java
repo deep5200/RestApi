7bb931c82708cfa576176b2597dc90378b7d3bcc
XJ892EC <XJ892EC@IN3234431W1.ey.net>
2022-05-19 02:20:38 +0530

    Updated Delete lead

--- src/main/java/restapi/Delete_Lead.java
+++ src/main/java/restapi/Delete_Lead.java
@@ -6,19 +6,17 @@ import io.restassured.response.Response;
 public class Delete_Lead {
 
 	public static void main(String[] args) {
-]
 		RestAssured.useRelaxedHTTPSValidation();
 
 		RestAssured.baseURI = "https://ap16.salesforce.com/services/data/v20.0/sobjects/lead/00Q5g00000D1fnDEAR";
 
-		RestAssured.authentication = RestAssured.oauth2
-				("00D5g000007oRrU!AQkAQBy5nlmyk84BMqX_6rmCyWL3l6CCHN0LogNoTU2rU0ZQAPaLkFq_Snqn.kunecO1Q7ezXctKtfS4ybl1l08_aKvYg2JN");
+		RestAssured.authentication = RestAssured.oauth2(
+				"00D5g000007oRrU!AQkAQBy5nlmyk84BMqX_6rmCyWL3l6CCHN0LogNoTU2rU0ZQAPaLkFq_Snqn.kunecO1Q7ezXctKtfS4ybl1l08_aKvYg2JN");
 
 		Response response = RestAssured.given().delete();
-		
-		
+
 		response.prettyPrint();
-		
+
 	}
 
 }
